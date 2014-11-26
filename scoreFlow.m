21f160bc839dc0d9c302802c857ff1e60905319e
Felix Sun <fephsun@gmail.com>
2014-11-25 21:24:03 -0500

    Working flow scorer - takes into account the fact that ground truth is corrupted somewhat.

--- scoreFlow.m
+++ scoreFlow.m
@@ -1,7 +1,10 @@
 function [ score ] = scoreFlow( flowX, flowY, groundTruth )
     groundTruthX = groundTruth(:,:,1);
     groundTruthY = groundTruth(:,:,2);
-    [width, height] = size(flowX);
+    [height, width] = size(flowX);
+    
+    groundTruthX(abs(groundTruthX) > 1000) = 0;
+    groundTruthY(abs(groundTruthY) > 1000) = 0;
     
     errorDist = ((flowX - groundTruthX).^2 + (flowY - groundTruthY).^2).^0.5;
     score = sum(sum(errorDist)) / width / height;
