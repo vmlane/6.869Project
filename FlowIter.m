c911ea1dfbce81691578430a4bc61732a67b0ecd
Felix Sun <fephsun@gmail.com>
2014-12-07 17:07:15 -0500

    Added charbonnier penalty

--- FlowIter.m
+++ FlowIter.m
@@ -1,4 +1,4 @@
-function [ u, v ] = FlowIter( img1, img2, laplacian)
+function [ u, v ] = FlowIter( img1, img2, laplacian, mode)
     
     % img1 and img2 assumed to be grayscale floating point images
 
@@ -15,12 +15,23 @@ function [ u, v ] = FlowIter( img1, img2, laplacian)
         Mrow = reshape(M, [], 1);
         sparseM = spdiags(Mrow, 0, length(Mrow), length(Mrow));
     end
-
+    function [ out ] = charb(x)
+        epsilon = 0.01;
+        out = x .* (x.^2 + epsilon) .^ -0.5;
+    end
+    function [ out ] = quad(x)
+        out = x;
+    end
+    if mode == 2
+        psi = @(x) charb(x);
+    else
+        psi = @(x) quad(x);
+    end
     % Compute linear flow operator
-    A = [diagSparse(dx.*dx) + alpha * laplacian, diagSparse(dx .* dy);
-         diagSparse(dx .* dy), diagSparse(dy.^2) + alpha * laplacian];
-    b = -[reshape(dx .* dt, [], 1); ...
-          reshape(dy .* dt, [], 1)];
+    A = [diagSparse(psi(dx.*dx)) + alpha * laplacian, diagSparse(psi(dx .* dy));
+         diagSparse(psi(dx .* dy)), diagSparse(psi(dy.^2)) + alpha * laplacian];
+    b = -[reshape(psi(dx .* dt), [], 1); ...
+          reshape(psi(dy .* dt), [], 1)];
     deltaFlow = A \ b; % solve linear equation
     u = reshape(deltaFlow(1:height*width, :), height, width);
     v = reshape(deltaFlow(height*width+1:2*height*width, :), height, width);
--- flowNIter.m
+++ flowNIter.m
@@ -1,17 +1,24 @@
-function [ u, v ] = flowNIter(im1, im2, n)
+function [ u, v ] = flowNIter(im1, im2, n, mode)
 % Calls FlowIter n times.
+% mode: 1 = quadratic, 2 = charbonnier
 [height, width] = size(im1);
 u = zeros(size(im1));
 v = zeros(size(im1));
 warpedIm1 = im1;
-% Make laplacian operator
-laplacian = makeLaplacian(width, height, im1);
-    
+global groundTruth;
+laplacian = makeLaplacian(width, height, u, v, mode);
+
 for i = 1:n
-    [du, dv] = FlowIter(warpedIm1, im2, laplacian);
+    if mode ~= 1
+        laplacian = makeLaplacian(width, height, u, v, mode);
+    end
+    [du, dv] = FlowIter(warpedIm1, im2, laplacian, mode);
     u = u + du;
     v = v + dv;
+    u = medfilt2(u);
+    v = medfilt2(v);
     warpedIm1 = applyFlow(im1, u, v);
+    scoreFlow(-u, -v, groundTruth)
 end
 
 end
