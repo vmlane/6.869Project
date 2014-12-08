075b265c0d90e07b60af38e6143dfe4a745e7e06
Felix Sun <fephsun@gmail.com>
2014-12-07 22:16:51 -0500

    Test suite for all pics

--- runner.m
+++ runner.m
@@ -1,25 +1,30 @@
 addpath('./flow-code-matlab');
+% titles = {'Dimetrodon', 'Grove2', 'Grove3', 'Hydrangea', 'RubberWhale', 'Urban2', 'Urban3', 'Venus'};
+titles = {'Urban2'};
+nTitles = length(titles);
+results = [];
+for i = 1:nTitles
+    im1 = double(rgb2gray(imread(['other-data/' titles{i} '/frame10.png'])))/255;
+    im2 = double(rgb2gray(imread(['other-data/' titles{i} '/frame11.png'])))/255;
+    groundTruth = readFlowFile(['./other-gt-flow/' titles{i} '/flow10.flo']);
+    groundTruth(abs(groundTruth) > 1000) = 0;
 
-im1 = double(rgb2gray(imread('other-data/RubberWhale/frame10.png')))/255;
-im2 = double(rgb2gray(imread('other-data/RubberWhale/frame11.png')))/255;
-global groundTruth;
-groundTruth = readFlowFile('./other-gt-flow/RubberWhale/flow10.flo');
-groundTruth(abs(groundTruth) > 1000) = 0;
+    options = struct();
 
-options = struct();
+    options.numPyramidLevels = 5;
+    options.numGncLevels = 2;
+    options.ratio = .5;
+    options.sigma = sqrt(1/options.ratio)/sqrt(2);
+    options.gncRatio = 1/1.25;
+    options.gncSigma = sqrt(1/options.gncRatio)/sqrt(2);
+    options.alpha = 0.1;
+    options.charbonnier = 1;
 
-options.numPyramidLevels = 4;
-options.numGncLevels = 2;
-options.ratio = .5;
-options.sigma = sqrt(1/options.ratio)/sqrt(2);
-options.gncRatio = 1/1.25;
-options.gncSigma = sqrt(1/options.gncRatio)/sqrt(2);
-options.alpha = 0.1;
-options.charbonnier = 1;
+    options.numGncIters = 1;
+    options.numWarpIters = 5;
 
-options.numGncIters = 2;
-options.numWarpIters = 5;
-
-[u, v] = computeFlow(im1,im2,options);
-imshow(VisualizeFlow(u, v, 6));
-score = scoreFlow(u, v, groundTruth)
+    [u, v] = computeFlow(im1,im2,options);
+    imshow(VisualizeFlow(u, v, 6));
+    score = scoreFlow(u, v, groundTruth);
+    results = [results score]
+end
